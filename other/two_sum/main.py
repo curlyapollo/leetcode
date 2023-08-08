805f3d920aafa3c40ead31918bb04b1fe5be2eb7
Leonid Karlinskiy <95444064+curlyapollo@users.noreply.github.com>
2023-08-08 15:24:04 +0300

    Update main.py

--- other/two_sum/main.py
+++ other/two_sum/main.py
@@ -3,6 +3,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
         for i in range(len(nums)):
-            if target - nums[i] in nums[i + 1:]:
-                return [i, nums[i + 1:].index(target - nums[i]) + i + 1]
+            if target - nums[i] in indices:
+                return [indices[target - nums[i]], i]
+            indices[nums[i]] = i
