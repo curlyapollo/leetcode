039e1437537b0a95057bd1d0b98d7bee3bafe502
Leonid Karlinskiy <95444064+curlyapollo@users.noreply.github.com>
2023-08-21 01:17:02 +0300

    Update main.py

--- linked_lists/linked_list_cycle/main.py
+++ linked_lists/linked_list_cycle/main.py
@@ -8,14 +8,6 @@ from typing import Optional
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        while head:
-            if head == 1e6:
-                return True
-            head.val = 1e6
-            head = head.val
-        return False
-
-    def hasCycle2(self, head: Optional[ListNode]) -> bool:
         fast = head
         while fast and fast.next:
             head = head.next
